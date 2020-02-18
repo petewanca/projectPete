d5b839883adff6b9fe1ef5863fcbbf3445e05bd5
Pete Wanca <pwanca@xsolis.corp>
2020-02-18 09:51:33 -0600

    adds body parser

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 // dependencies
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 require('dotenv').config();
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+// support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // declare port #
 const port = 5000;
